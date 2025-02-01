30698b21e14b9b8051f6350c6ff9fbd8cb9eb9bc
answer0424 <wonwon123123@naver.com>
2025-02-01 14:56:40 +0900

    13. Linux 파일 찾기, 내용 비교, 링크, 압축 -완-

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738388785',resources: [
+    cacheName: 'chirpy-1738389395',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
