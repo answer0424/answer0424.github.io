5c77230b51893e46ee60a6823a5973ca8c6e2b90
answer0424 <wonwon123123@naver.com>
2025-02-01 15:01:08 +0900

    14. Linux Shell 관련 명령어어

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738389395',resources: [
+    cacheName: 'chirpy-1738389659',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
