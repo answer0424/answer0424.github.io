e18392abd64f014c7b462eb21e345bbb2dc3f2d9
answer0424 <wonwon123123@naver.com>
2025-02-01 14:25:43 +0900

    09.Linux 날짜와 시간 명령어 -완-

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738387181',resources: [
+    cacheName: 'chirpy-1738387517',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
