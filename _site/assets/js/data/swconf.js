b5f1175b24faf3c4ea35d6be521a2bfca58f551e
answer0424 <wonwon123123@naver.com>
2025-02-01 17:06:56 +0900

    feat: 수정 4

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738392369',resources: [
+    cacheName: 'chirpy-1738397178',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
