79b0846fa1a18b2e840815d636cc36edd786afbc
answer0424 <wonwon123123@naver.com>
2025-02-01 14:44:57 +0900

    11. Linux 파일 내용 확인, 편집 명령어 -완-

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738388131',resources: [
+    cacheName: 'chirpy-1738388658',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
