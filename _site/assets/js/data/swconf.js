a6b2805aacbfed28ed869afb98b4d7f1c060f2e1
answer0424 <wonwon123123@naver.com>
2025-02-01 14:35:51 +0900

    10. Linux 파일과 디렉터리 복사 및 이동 -완-

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738387517',resources: [
+    cacheName: 'chirpy-1738388131',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
