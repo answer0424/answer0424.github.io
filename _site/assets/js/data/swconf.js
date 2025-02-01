b277b4149899cc61128892c97529ebee425cba3d
answer0424 <wonwon123123@naver.com>
2025-02-01 14:02:11 +0900

    07. Linux 리눅스 서버 복제 및 파일, 디렉터리 생성 명령어 -완-

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738384020',resources: [
+    cacheName: 'chirpy-1738386108',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
