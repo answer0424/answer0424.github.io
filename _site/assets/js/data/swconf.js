62c59a7c7709f905ff30586005b38ab506a03c12
answer0424 <wonwon123123@naver.com>
2025-02-03 09:44:17 +0900

    feat: websocket01 -완-

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738397508',resources: [
+    cacheName: 'chirpy-1738543432',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
