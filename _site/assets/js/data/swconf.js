37e0f8ff4fbbe9c8bc2331734c76b7c8d00efc75
answer0424 <wonwon123123@naver.com>
2025-02-04 08:42:32 +0900

    feat: update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738543432',resources: [
+    cacheName: 'chirpy-1738625561',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
