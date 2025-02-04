8f1ea16a91061cd10350508dc5e0b8169f99b15c
answer0424 <wonwon123123@naver.com>
2025-02-04 19:29:03 +0900

    feat: 재수정2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738664905',resources: [
+    cacheName: 'chirpy-1738664935',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
