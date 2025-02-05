4166ddbde195555855476d766f1bacf2b0ceaa71
answer0424 <wonwon123123@naver.com>
2025-02-05 09:05:01 +0900

    feat: 22. Linux 허가권 -완-

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738712567',resources: [
+    cacheName: 'chirpy-1738713871',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
