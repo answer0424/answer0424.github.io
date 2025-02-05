58b6da23e220b39b1aee54c10896f7277810e73e
answer0424 <wonwon123123@naver.com>
2025-02-06 08:51:39 +0900

    feat: 24. Linux 디스크 쿼터

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738799374',resources: [
+    cacheName: 'chirpy-1738799459',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
