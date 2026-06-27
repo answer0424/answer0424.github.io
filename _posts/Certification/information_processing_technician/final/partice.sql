480288ea045700c384d44b79c0190913ba77d4f8
answer0424 <wonwon123123@naver.com>
2026-06-27 16:15:16 +0900

    feat: normal form

--- _posts/Certification/information_processing_technician/final/partice.sql
+++ _posts/Certification/information_processing_technician/final/partice.sql
@@ -39,3 +39,26 @@ update set
 alter create drop 
 select delete insert update
 grant revoek
+
+11 17
+
+200 3 1
+
+select * from Product where price is null order by name asc;
+
+and asc not
+
+이% desc
+
+학과코드, 학과, 교수명, 학생수 학생수 <= 30
+
+SELECT DISTINT 과목 FROM 학생 WHERE 학년 >= 3
+
+and distinct in
+
+SELECT ID, NAME FROM CUSTOMER;
+SELECT DISTINCT GRADE FROM CUSTOMER;
+SELECT NAME FROM CUSTOMER WHERE AGE IS NULL;
+SELECT NAME FROM CUSTOMER WHERE AGE IS NOT NULL;
+
+AND BETWEEN
